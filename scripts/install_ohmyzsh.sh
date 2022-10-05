2efd315bb1275352cda1fd8eea79bf8efcff7b68
shawshary <xinyuloveswork@163.com>
2022-10-05 11:14:21 +0800

    [ADD] make zsh the default login shell

--- scripts/install_ohmyzsh.sh
+++ scripts/install_ohmyzsh.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-# Install oh-my-zsh
+################################################################################
+#                     Install and Configure the on-my-zsh                      #
+################################################################################
+
 
 #############################
 #  Check the prerequisites  #
 #############################
-
 
 function check_prerequsites() {
   # TODO: use loop
@@ -28,8 +30,24 @@ function check_prerequsites() {
   return 0;
 }
 
+function change_login_shell() {
+  echo "Make zsh the valid login shell.";
+  echo "$(which zsh)" >> /etc/shells;
+  [[ $? == 0 ]] && echo "Make zsh the valid login shell done.";
+
+  [[ -z $(which chsh) ]] || chsh -s $(which zsh);
+  [[ $? == 0 ]] && echo "Change login shell done.";
+}
+
 check_prerequsites
-if [[ $? == 0 ]]; then
+if [[ ($? == 0) && (-z $(which zsh)) ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [[ ! -z $(which zsh) ]]; then
+  echo "zsh found"
+  echo "Change the default login shell: START"
+  change_login_shell;
+  echo "Change the default login shell: END"
 fi
 
